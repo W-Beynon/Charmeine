4e020c610434451c25785236ac3f1d3a2f16a2bf
Exxilur <57550148+Hubbleablubble@users.noreply.github.com>
2020-11-07 10:35:50 -0600

    Tilemap Test Stuff

---
+++ Puzzle Time/Assets/_Scripts/MouseInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+    public void mouseGetPos(Camera cam)
+    {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2D = Physics2D.GetRayIntersection(ray);
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+            if (hit2D.collider != null)
+            {
+                Debug.Log("We hit " + hit2D.collider.name + " at position " + hit2D.point);
+            }
+   }
+}
---
+++ Puzzle Time/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+	public Tilemap map;
+	Camera cam;
+//	MouseInput msInp;
+//
+	private Vector3 destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	destination = transform.position;
+        cam = Camera.main;
+//	msInp = gameObject.AddComponent<MouseInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+	{
+		mouseGetPos();	
+	}
+    }
+
+    void mouseGetPos()
+    {
+/*            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2D = Physics2D.GetRayIntersection(ray);
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+            if (hit2D.collider != null)
+            {
+                Debug.Log("We hit " + hit2D.collider.name + " at position " + hit2D.point);
+            }*/
+
+	    Vector2 mousePos = Input.mousePosition;
+	    mousePos = cam.ScreenToWorldPoint(mousePos);
+	    Vector3Int gridPosition = map.WorldToCell(mousePos);
+	    if(map.HasTile(gridPosition)){
+		destination = gridPosition;//map.GetTile(gridPosition).position;
+	    }
+   }
+
+}

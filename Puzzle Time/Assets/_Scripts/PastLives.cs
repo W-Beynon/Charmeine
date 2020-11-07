5de404fbdb81e4370a77d0f605c945c9f8b1bdf0
Exxilur <57550148+Hubbleablubble@users.noreply.github.com>
2020-11-07 16:16:02 -0600

    TIME

--- Puzzle Time/Assets/_Scripts/PastLives.cs
+++ Puzzle Time/Assets/_Scripts/PastLives.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PastLives : MonoBehaviour
 {
+	public int amtMoves = 75;
+	public TimeKeeper getTime;
+	public float movementSpeed;
+
+	private Vector3[] pastMoves;
+	private int rounds;
+	private int currRound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pastMoves = new Vector3[amtMoves];
+	currRound = 0;
+	gameObject.SetActive(false);
+    }
+
+    public void startIt(){
+	gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     //   movement();
+    }
+
+    public void addRound(Vector3 pos){
+	pastMoves[currRound++] = pos;
+    }
+
+    public void movement(){
+	transform.position = Vector3.MoveTowards(transform.position, pastMoves[getTime.getRound()], movementSpeed * Time.deltaTime);
     }
 }

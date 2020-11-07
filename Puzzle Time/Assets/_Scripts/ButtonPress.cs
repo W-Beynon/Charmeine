291c758201eca67c74ad37cd70bf6bdb23b030b3
CM-beynon <35342832+CM-beynon@users.noreply.github.com>
2020-11-07 12:11:22 -0600

    Basic Action Inheritance
    (And button press)

---
+++ Puzzle Time/Assets/_Scripts/ButtonPress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public GameObject attachedObject;
+    public bool pressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed)
+        {
+            attachedObject.GetComponent<Action>().performAction();
+        }
+    }
+}

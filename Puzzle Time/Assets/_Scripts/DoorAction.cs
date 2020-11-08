c8dfbfb9ea1e76b8bec1aac47142a140e868c617
CM-beynon <35342832+CM-beynon@users.noreply.github.com>
2020-11-08 17:48:31 -0600

    Level 1 GOOOOOOOO

--- Puzzle Time/Assets/_Scripts/DoorAction.cs
+++ Puzzle Time/Assets/_Scripts/DoorAction.cs
@@ -9,19 +9,25 @@ public class DoorAction : Action
     public Tilemap wall;
     private Vector3Int doorPos;
     private TileBase door;
+    private TileBase door2;
+    private Tilemap postDoors;
     private Tilemap doors;
+    private GameObject tileDoors;
+    private TilemapRenderer doorsRender;
 
     // Start is called before the first frame update
     void Start()
     {
         lifted = false;
         wall = (Tilemap)GameObject.Find("Wall").GetComponent(typeof(Tilemap));
+        postDoors = (Tilemap)GameObject.Find("PostDoors").GetComponent(typeof(Tilemap));
         doors = (Tilemap)GameObject.Find("Doors").GetComponent(typeof(Tilemap));
         doorPos = wall.WorldToCell(transform.position);
         doorPos.x = doorPos.x;
         doorPos.y = doorPos.y;
         doorPos.z = 0;
         door = wall.GetTile(doorPos);
+        door2 = doors.GetTile(doorPos);
     }
 
     // Update is called once per frame
@@ -36,14 +42,13 @@ public class DoorAction : Action
             // drops door
             lifted = false;
             wall.SetTile(doorPos, door);
-            doors.SetTile(doorPos, null);
+            postDoors.SetTile(doorPos, null);
         }
         else
         {
             // lifts door
-            Debug.Log(door);
             lifted = true;
-            doors.SetTile(doorPos, door);
+            postDoors.SetTile(doorPos, door2);
             wall.SetTile(doorPos, null);
         }
     }

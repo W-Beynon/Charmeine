8449a647a5cf0f9229d17519cf675d96ab1d5e09
CM-beynon <35342832+CM-beynon@users.noreply.github.com>
2020-11-08 19:08:04 -0600

    Next Level

---
+++ Puzzle Time/Assets/_Scripts/Replay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Replay : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("FUCK");
+    }
+}

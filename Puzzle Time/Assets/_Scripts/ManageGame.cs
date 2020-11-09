2ef683ad8f403441df0f28e9beda1076b2d502e5
Exxilur <57550148+Hubbleablubble@users.noreply.github.com>
2020-11-08 19:24:29 -0600

    Changes!!!

---
+++ Puzzle Time/Assets/_Scripts/ManageGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Q)){
+		Debug.Log("Quitted!");
+		Application.Quit();
+	}
+    }
+}
